3a3e3587e4cd2ff17debbceccda84253ecc41cb2
Sez-e <marfa29savina@gmail.com>
2026-04-28 00:25:50 +0300

    Register user routes

--- src/main.py
+++ src/main.py
@@ -3,7 +3,8 @@ from fastapi import FastAPI
 from src.auth.endpoints import router as auth_router
 from src.core.base_model import Base
 from src.core.database import engine
-from src.user.models import User  # noqa: F401
+from src.user.endpoints import router as user_router
+from src.user.models import User, UserContacts, UserExperience  # noqa: F401
 
 Base.metadata.create_all(bind=engine)
 
@@ -11,3 +12,4 @@ app = FastAPI(title="passproof")
 
 
 app.include_router(auth_router, prefix="/auth", tags=["auth"])
+app.include_router(user_router, prefix="/users", tags=["users"])
