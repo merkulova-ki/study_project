3c8d6376476ccfb4b6f7c95f8c33a030c53816a6
Sez-e <marfa29savina@gmail.com>
2026-04-28 01:05:25 +0300

    Register company routes

--- src/main.py
+++ src/main.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI
 
 from src.auth.endpoints import router as auth_router
+from src.company.endpoints import router as company_router
+from src.company.models import (  # noqa: F401
+    Company,
+    CompanyInvitation,
+    CompanyMember,
+    CompanyTasks,
+    CompanyWallet,
+)
 from src.core.base_model import Base
 from src.core.database import engine
 from src.user.endpoints import router as user_router
@@ -13,3 +21,4 @@ app = FastAPI(title="passproof")
 
 app.include_router(auth_router, prefix="/auth", tags=["auth"])
 app.include_router(user_router, prefix="/users", tags=["users"])
+app.include_router(company_router, prefix="/companies", tags=["companies"])
